b158a5586edda45ae33e567d34058f80b0f68ed5
Kohei Watanabe <xbeef29@gmail.com>
2017-06-26 21:23:53 +0900

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub extern crate compacts_prim as prim;
 pub extern crate compacts_bits as bits;
 pub extern crate compacts_dict as dict;
+
+// pub use prim::UnsignedInt;
+// pub use bits::{Vec32, Vec64};
+// pub use dict::{BitDict, Dict};
